9ed8651dac4251e9b15b23313318ec91f9e47d29
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2025-06-20 15:47:28 -0300

    Atualiza dependências do Tailwind CSS e PostCSS no package.json e package-lock.json, ajustando a versão do tailwindcss para 3.4.17. Adiciona configuração do PostCSS em um novo arquivo postcss.config.js e modifica a importação no tailwind.config.js para utilizar '@nrwl/angular/tailwind'.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
 const { join } = require('path');
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
