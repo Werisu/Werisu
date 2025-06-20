209e3865993e3824e80e720a2de162b7ac32f753
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2025-06-20 15:54:10 -0300

    Remove dependências do Tailwind CSS do package.json e package-lock.json, simplificando a configuração do projeto. Atualiza a configuração do Tailwind para utilizar um padrão de conteúdo mais direto no tailwind.config.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
-const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
-const { join } = require('path');
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
